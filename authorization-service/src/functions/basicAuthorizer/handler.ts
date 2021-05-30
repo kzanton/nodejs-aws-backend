0e0c7819ea26cee9910da997ab3a665833a0cb22
Anton Kuzmenko <anton_kuzmenko@epam.com>
2021-05-31 02:01:28 +0300

    Refactor authorization handler

--- authorization-service/src/functions/basicAuthorizer/handler.ts
+++ authorization-service/src/functions/basicAuthorizer/handler.ts
@@ -6,38 +6,38 @@ import { APIGatewayTokenAuthorizerEvent, APIGatewayTokenAuthorizerHandler } from
 const basicAuthorizer: APIGatewayTokenAuthorizerHandler = async (event: APIGatewayTokenAuthorizerEvent) => {
   console.log('event', event);
 
-  if (event.type !== 'TOKEN') {
-    throw new Error('Unauthorized');
+  const encodedCreds = event.authorizationToken.split(' ')[1];
+
+  if (!encodedCreds) {
+    throw new Error('The token is incorrect');
   }
 
-  try {
-    const encodedCreds = event.authorizationToken.split(' ')[1];
-    const plainCreds = Buffer.from(encodedCreds, 'base64').toString('utf-8').split(':');
-    const username = plainCreds[0];
-    const password = plainCreds[1];
-    const accessPassword = process.env[username];
-    const effect = accessPassword && accessPassword === password ? 'Allow' : 'Deny';
+  const plainCreds = Buffer.from(encodedCreds, 'base64').toString('utf-8').split(':');
+  const username = plainCreds[0];
+  const password = plainCreds[1];
 
-    console.log(`username=${username} password=${password} effect=${effect}`);
-
-    return {
-      principalId: encodedCreds,
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: effect,
-            Resource: event.methodArn
-          }
-        ],
-      },
-    };
-
-  } catch (e) {
-    console.error(e.message);
-    throw new Error(`Unauthorized. ${e.message}`);
+  if (!username) {
+    throw new Error('The token is incorrect');
   }
+
+  const accessPassword = process.env[username];
+  const effect = accessPassword && accessPassword === password ? 'Allow' : 'Deny';
+
+  console.log(`username=${username} password=${password} effect=${effect}`);
+
+  return {
+    principalId: encodedCreds,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: event.methodArn
+        }
+      ],
+    },
+  };
 }
 
 export const main = middyfy(basicAuthorizer);
